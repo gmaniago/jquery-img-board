3e828b256442eb27068e7c01e789c8eafed5a224
Gladys Maniago <gfmaniago@gmail.com>
2015-09-18 17:17:39 -0500

    complete normal mode

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -3,24 +3,43 @@
 
 $(document).ready(function () {
 
-	var $photoForm = $('#photoForm');
-	var $photoUrl = $('#photoUrl');
-	var $photoCaptipn = $('#photoCaption');
-	var $imageWindow = $('#imageWindow');
-	var url = 'http://tiyfe.herokuapp.com/collections/gmaniago';
+    var $photoForm = $('#photoForm');
+    var $photoUrl = $('#photoUrl');
+    var $photoCaption = $('#photoCaption');
+    var $imageWindow = $('#imageWindow');
+    var url = 'http://tiyfe.herokuapp.com/collections/g1';
 
-	$('#plusBtn').click(function () {
-		$photoForm.toggle('slow');
-	});
-	function photos() {
-		$photoUrl = $photoUrl.val('');
-		$photoCaption = $photoCaption.val('');
-	}
-	$('#cancelBtn').click(function (e) {
-		e.preventDefault();
-		photos();
-		$photoForm.hide('slow');
-	});
+    $('#plusBtn').click(function () {
+        $photoForm.toggle('slow');
+    });
+    function photos() {
+        $photoUrl = $photoUrl.val('');
+        $photoCaption = $photoCaption.val('');
+    }
+    $('#cancelBtn').click(function (e) {
+        e.preventDefault();
+        photos();
+        $photoForm.hide('slow');
+    });
+    $('#addBtn').click(function (e) {
+        e.preventDefault();
+        $photoForm.hide('slow');
+        var imgUrl = $photoUrl.val();
+        var caption = $photoCaption.val();
+        console.log(imgUrl);
+        console.log(caption);
+        $.post(url, {
+            imgUrl: imgUrl,
+            caption: caption
+        }, function (data) {
+            $imageWindow.append('<div class="images"><img src="' + data.imgUrl + '"></div><div class="captions">' + data.caption + '</div>');
+        });
+    });
+    $.get(url, function (imgcap) {
+        imgcap.forEach(function (imgcap) {
+            $imageWindow.append('<div class="images"><img src="' + imgcap.imgUrl + '"></div><div class="captions">' + imgcap.caption + '</div>');
+        });
+    }, 'json');
 });
 
 },{}]},{},[1])
--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
 
 	var $photoForm = $('#photoForm');
 	var $photoUrl = $('#photoUrl');
-	var $photoCaptipn = $('#photoCaption');
+	var $photoCaption = $('#photoCaption');
 	var $imageWindow = $('#imageWindow')
-	var url = 'http://tiyfe.herokuapp.com/collections/gmaniago';
+	var url = 'http://tiyfe.herokuapp.com/collections/g1';
 
 	$('#plusBtn').click(function() {
 		$photoForm.toggle('slow');
@@ -20,5 +20,35 @@ $(document).ready(function() {
         photos();
         $photoForm.hide('slow');
     });
+    $('#addBtn').click(function(e) {
+        e.preventDefault();
+        $photoForm.hide('slow');
+        var imgUrl = $photoUrl.val();
+        var caption = $photoCaption.val();
+        console.log(imgUrl);
+        console.log(caption);
+        $.post(
+            url, {
+                imgUrl: imgUrl,
+                caption: caption
+            },
+            function(data) {
+                $imageWindow.append('<div class="images"><img src="'+data.imgUrl+
+                    '"></div><div class="captions">'+data.caption+'</div>')
+            }
+        );
+	});
+	$.get(
+        url,
+        function(imgcap) {
+            imgcap.forEach(function(imgcap) {
+                $imageWindow.append('<div class="images"><img src="'+imgcap.imgUrl+
+                    '"></div><div class="captions">'+imgcap.caption+'</div>')
+            })
+        },
+        'json'
+    );
 
 });
+
+
